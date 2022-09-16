ae01d1863a8d4f84de849b4eedcf7b1436dd2849
Fabian Batioja <f.batioja@uniandes.edu.co>
2022-09-15 20:34:26 -0500

    Update settings.py

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "10.0.103.57"]
+ALLOWED_HOSTS = ["localhost", "18.204.42.94"]
 
 
 # Application definition
